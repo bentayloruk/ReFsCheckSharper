4578b6a9cd41ebbafa25e83731cdef8de3b2338e
Ben Taylor <ben.taylor.uk@gmail.com>
2017-02-24 09:57:35 +0000

    So I walk up the street whistlin this

---
+++ ReFsCheckSharper/ReproPropertyTest.fs
@@ -0,0 +1,13 @@
+﻿namespace ReFsCheckSharper
+
+open System
+open FsCheck
+open FsCheck.Xunit
+
+[<Properties>]
+module ReproPropertyTest =
+
+    [<Property>]
+    let anyOldIntTest (x:int) =
+        // This is the sole test in this project.
+        x <= Int32.MaxValue && x >= Int32.MinValue
